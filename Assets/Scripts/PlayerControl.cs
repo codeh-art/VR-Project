0d6fe8c129e589f524dfdcddf02f9839db2573ce
Raihauti <teiki.rai@hotmail.com>
2019-12-02 13:14:23 +0100

    Manipulation objet

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class PlayerControl : MonoBehaviour
+{
+    void Update()
+    {
+        Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
+        transform.position += transform.forward * Time.deltaTime * 2 * pos.y;
+        transform.position += transform.right * Time.deltaTime * 2 * pos.x;
+    }
+}
