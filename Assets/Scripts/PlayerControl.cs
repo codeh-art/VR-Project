eba926ee1b5876beb3fe65aab148ec56b80ab7db
Raihauti <teiki.rai@hotmail.com>
2019-12-18 21:05:50 +0100

    Followers

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,10 +1,35 @@
 ﻿using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
+    Vector3 pos2;
     void Update()
     {
         Vector2 pos = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad);
-        transform.position += transform.forward * Time.deltaTime * 2 * pos.y;
-        transform.position += transform.right * Time.deltaTime * 2 * pos.x;
+        if (Input.GetKey("z"))
+        {
+            pos2 = Vector3.forward;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos2 = Vector3.back;
+        }
+        if (Input.GetKey("q"))
+        {
+            pos2 = Vector3.left;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos2 = Vector3.right;
+        }
+        if(Input.GetKey("a"))
+        {
+            pos2 = Vector3.down;
+        }
+        if (Input.GetKey("e"))
+        {
+            pos2 = Vector3.up;
+        }
+        transform.position += Time.deltaTime * 2 * pos2;
+        pos2 = Vector3.zero;
     }
 }
