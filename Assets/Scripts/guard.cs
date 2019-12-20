8707fa7ce146d4b2f84bbe3dbd3f03fa9ea80d69
Mira Skendraoui <mirajane.aye@hotmail.fr>
2019-12-20 10:20:23 +0100

    Final look
    Added terrain, houses, furniture, guard random rotation , etc.

---
+++ Assets/Scripts/guard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class guard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.rotation = Random.rotation; 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
